20e522e31a81ead7627579a729deecbce3befaf0
Adam Andersson <adam.m.andersson@gmail.com>
2019-05-02 15:47:56 +0200

    This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-############################
-# .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
-
-########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="bashrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
-
-##########
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
