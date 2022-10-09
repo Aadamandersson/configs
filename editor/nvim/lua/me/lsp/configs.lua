43734ca1e31edb45738aa8d1e1aca55a68490bc5
Adam Andersson <adam.m.andersson@gmail.com>
2022-10-09 18:03:38 +0200

    remove old code

--- editor/nvim/lua/me/lsp/configs.lua
+++ editor/nvim/lua/me/lsp/configs.lua
@@ -11,7 +11,6 @@ end
 
 mason.setup()
 mason_lsp_cfg.setup()
-
 mason_lsp_cfg.setup_handlers {
     function(server_name)
          local opts = {
@@ -21,34 +20,9 @@ mason_lsp_cfg.setup_handlers {
          if server_name == "sumneko_lua" then
             local sumneko_opts = require("me.lsp.settings.sumneko_lua")
             opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-            --sumneko_opts["on_attach"] = opts.on_attach
-            --sumneko_opts["capabilities"] = opts.capabilities
-            --lspconfig[server_name].setup(sumneko_opts)
          end
          lspconfig[server_name].setup(opts)
     end
-    -- function(server_name)
-    --     local opts = {
-    --         on_attach = require("me.lsp.handlers").on_attach,
-    --         capabilities = require("me.lsp.handlers").capabilities
-    --     }
-    --     local has_cust_opts, cust_opts = pcall(require, "me.lsp.settings." .. server_name)
-    --     if has_cust_opts then
-    --         opts = vim.tbl_deep_extend("force", opts, cust_opts)
-    --     end
-    --     lspconfig[server_name].setup(opts)
-    -- end,
-    -- ["sumneko_lua"] = function()
-    --     local opts = {
-    --         on_attach = require("me.lsp.handlers").on_attach,
-    --         capabilities = require("me.lsp.handlers").capabilities
-    --     }
-    --     local has_opts, cust_opts = pcall(require, "me.lsp.settings.sumneko_lua")
-    --     if has_opts then
-    --         opts = vim.tbl_deep_extend("force", opts, cust_opts)
-    --     end
-    --     lspconfig.sumneko_lua.setup(opts)
-    -- end,
 }
 
 
