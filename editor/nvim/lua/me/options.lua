471c0a37903afedcb8d165acbc1f73fb3db1ee13
Adam Andersson <adam.m.andersson@gmail.com>
2022-10-09 14:31:34 +0200

    add options

---
+++ editor/nvim/lua/me/options.lua
@@ -0,0 +1,39 @@
+local opts = {
+	tabstop = 4,
+	shiftwidth = 4,
+	softtabstop = 4,
+    cmdheight = 2,
+    scrolloff = 8,
+    updatetime = 300,
+    showmatch = true,
+	expandtab = true,
+	number = true,
+    relativenumber = true,
+    undofile = true,
+    splitright = true,
+    splitbelow = true,
+    wrap = false,
+}
+
+for k, v in pairs(opts) do
+	vim.opt[k] = v
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
