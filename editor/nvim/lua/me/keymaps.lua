f4f597216ae3738c12d4a7a98f29b37b71268613
Adam Andersson <adam.m.andersson@gmail.com>
2023-03-28 07:59:54 +0200

    sane paste

--- editor/nvim/lua/me/keymaps.lua
+++ editor/nvim/lua/me/keymaps.lua
@@ -15,6 +15,7 @@ keymap("", "<left>", "<nop>", opts)
 keymap("", "<right>", "<nop>", opts)
 keymap("", "<leader>co", ":copen<CR>", opts)
 keymap("", "<leader>cc", ":cclose<CR>", opts)
+keymap("x", "<leader>p", '"_dP', opts)
 
 -- `nvim_set_keymap` does not accept lua functions, so we use `vim.keymap.set` instead.
 vim.keymap.set("n", "<leader>e", function()
