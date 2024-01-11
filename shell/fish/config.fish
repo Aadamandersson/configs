adee72662e457f8e00c58d35d907c71d92af4aec
Adam Andersson <adam.m.andersson@gmail.com>
2024-01-11 16:34:40 +0100

    .

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -4,3 +4,9 @@ end
 
 string match -q "$TERM_PROGRAM" "vscode"
 and . (code --locate-shell-integration-path fish)
+
+set -gx LANG en_US.UTF-8
+
+alias vim /opt/homebrew/bin/nvim
+alias python /opt/homebrew/bin/python3
+alias python3 /opt/homebrew/bin/python3
