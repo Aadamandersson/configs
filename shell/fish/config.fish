5905542061b72079067925dcc5d095994a9e7aa9
Adam Andersson <adam.m.andersson@gmail.com>
2025-07-08 21:10:51 +0200

    .

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -1,12 +1,5 @@
+set -g fish_greeting
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
-string match -q "$TERM_PROGRAM" "vscode"
-and . (code --locate-shell-integration-path fish)
-
-set -gx LANG en_US.UTF-8
-fish_add_path /opt/homebrew/opt/python/libexec/bin
-# alias vim /opt/homebrew/bin/nvim
-# alias python /opt/homebrew/bin/python3
-# alias python3 /opt/homebrew/bin/python3
