ddf1d6e6680eed29e50d2390e22341b16f195d9d
Adam Andersson <adam.m.andersson@gmail.com>
2025-07-08 18:02:51 +0200

    lazy

---
+++ editor/nvim/lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+vim.keymap.set("n", "<leader>fN", ':e <C-R>=expand("%:p:h") . "/" <cr>', {
+  desc = "New file (cwd)",
+})
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set({ "n", "v", "i" }, "<up>", "<nop>")
+vim.keymap.set({ "n", "v", "i" }, "<down>", "<nop>")
+vim.keymap.set({ "n", "v", "i" }, "<left>", "<nop>")
+vim.keymap.set({ "n", "v", "i" }, "<right>", "<nop>")
